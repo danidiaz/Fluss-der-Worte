e11ae8c6d3e7b9d88bfeb7504ecfe8cd732cb5f6
Daniel Diaz <diaz.carrete@facebook.com>
2017-06-27 10:09:54 +0200

    new direction

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,3 @@
-create database redefluss;
-
-grant all on redefluss.* to 'redefluss'@'localhost';
-
 create table redefluss.sentence (
     id   integer NOT NULL AUTO_INCREMENT,
     text VARCHAR(1000) NOT NULL,
